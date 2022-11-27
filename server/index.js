712959bebd37d82b12e152398f332d4b91a9353e
hafezfhmi <hafezfahmi2000@gmail.com>
2022-11-27 21:24:52 +0800

    Feat: Server - Add starter express server

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const cats = [
+  { id: 1, name: "coco" },
+  { id: 2, name: "milo" },
+  { id: 3, name: "snowy" },
+  { id: 4, name: "meowy" },
+  { id: 5, name: "jojo" },
+];
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
+
+app.get("/api/cats", (req, res) => {
+  res.json(cats);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${{ PORT }}`);
+});
