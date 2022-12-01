b4324aa3785be97e2f9111e9b418e855c273f1f8
hafezfhmi <hafezfahmi2000@gmail.com>
2022-12-02 02:09:02 +0800

    Feat: Server - Add middleware to protect routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
 
 const { postLogin, postLogout, postSignup } = require("../controllers/auth");
+const isAuth = require("../middleware/isAuth");
 
 router.post("/login", postLogin);
-router.post("/logout", postLogout);
+router.post("/logout", isAuth, postLogout);
 router.post("/signup", postSignup);
 
 module.exports = router;
