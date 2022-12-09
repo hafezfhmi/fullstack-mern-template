009ea728185176187637f8ca69a50a42d7d7273a
hafezfhmi <hafezfahmi2000@gmail.com>
2022-12-09 15:38:31 +0800

    Refactor: Server - Exclude password field from getAllUsers

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -2,7 +2,7 @@ const User = require("../models/users");
 
 exports.getAllUsers = async (req, res, next) => {
   try {
-    const users = await User.findAll();
+    const users = await User.findAll({ attributes: { exclude: ["password"] } });
     res.json(users);
   } catch (error) {
     next(error);
