fbefdc09b03ecebfbfa1c57fa6b17e33997323d5
hafezfhmi <hafezfahmi2000@gmail.com>
2022-12-14 04:59:29 +0800

    Feat: Client - Add error modal to sign up form and change error modal position to fixed

--- client/src/components/SignupForm/index.jsx
+++ client/src/components/SignupForm/index.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { useNavigate, Link } from "react-router-dom";
 import Button from "../Button";
+import ErrorModal from "../ErrorModal";
 import styles from "./signupForm.module.css";
 
-import { useField } from "../../hooks/index";
+import { useField, useError } from "../../hooks/index";
 import authServices from "../../services/auth";
 
 const SignupForm = () => {
@@ -14,6 +15,7 @@ const SignupForm = () => {
   const email = useField("email", "email");
   const password = useField("password", "password");
   const confirmPassword = useField("password", "confirmPassword");
+  const error = useError();
 
   let handleSignup = async (event) => {
     event.preventDefault();
@@ -28,50 +30,58 @@ const SignupForm = () => {
         confirmPassword.attributes.value
       );
       navigate("/signup-validation");
-    } catch (error) {
-      console.error(error);
+    } catch (err) {
+      error.handleError(true, err.response.data.error);
     }
   };
 
   return (
-    <form className={styles.authForm} onSubmit={handleSignup}>
-      <h1>Sign up</h1>
-      <div className={styles.nameWrapper}>
-        <div className={styles.inputWrapper}>
-          <label htmlFor="firstName">First name: </label>
-          <input {...firstName.attributes} />
+    <>
+      {error.error && (
+        <ErrorModal
+          errorMsg={error.errorMsg}
+          disableError={error.disableError}
+        />
+      )}
+      <form className={styles.authForm} onSubmit={handleSignup}>
+        <h1>Sign up</h1>
+        <div className={styles.nameWrapper}>
+          <div className={styles.inputWrapper}>
+            <label htmlFor="firstName">First name: </label>
+            <input {...firstName.attributes} />
+          </div>
+          <div className={styles.inputWrapper}>
+            <label htmlFor="lastName">Last name: </label>
+            <input {...lastName.attributes} />
+          </div>
         </div>
         <div className={styles.inputWrapper}>
-          <label htmlFor="lastName">Last name: </label>
-          <input {...lastName.attributes} />
+          <label htmlFor="username">Username: </label>
+          <input {...username.attributes} />
         </div>
-      </div>
-      <div className={styles.inputWrapper}>
-        <label htmlFor="username">Username: </label>
-        <input {...username.attributes} />
-      </div>
-      <div className={styles.inputWrapper}>
-        <label htmlFor="email">Email: </label>
-        <input {...email.attributes} />
-      </div>
-      <div className={styles.inputWrapper}>
-        <label htmlFor="password">Password: </label>
-        <input {...password.attributes} />
-      </div>
-      <div className={styles.inputWrapper}>
-        <label htmlFor="confirmPassword">Confirm password: </label>
-        <input {...confirmPassword.attributes} />
-      </div>
-      <Button
-        label="Sign up"
-        style={{ "margin-top": "1.2rem", width: "100%" }}
-      />
-      <div className={styles.extraWrapper}>
-        <p>
-          Have an account? <Link to={"/login"}>Log in</Link>
-        </p>
-      </div>
-    </form>
+        <div className={styles.inputWrapper}>
+          <label htmlFor="email">Email: </label>
+          <input {...email.attributes} />
+        </div>
+        <div className={styles.inputWrapper}>
+          <label htmlFor="password">Password: </label>
+          <input {...password.attributes} />
+        </div>
+        <div className={styles.inputWrapper}>
+          <label htmlFor="confirmPassword">Confirm password: </label>
+          <input {...confirmPassword.attributes} />
+        </div>
+        <Button
+          label="Sign up"
+          style={{ "margin-top": "1.2rem", width: "100%" }}
+        />
+        <div className={styles.extraWrapper}>
+          <p>
+            Have an account? <Link to={"/login"}>Log in</Link>
+          </p>
+        </div>
+      </form>
+    </>
   );
 };
 
