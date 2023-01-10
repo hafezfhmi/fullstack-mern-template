34420a6708f1f737d5b6a6e4b47a0b6a9f22b339
hafezfhmi <hafezfahmi2000@gmail.com>
2023-01-10 20:15:44 +0800

    Feat: Server - Update database connection configuration

--- server/index.js
+++ server/index.js
@@ -9,14 +9,10 @@ const PORT = 3001;
 
 const server = http.createServer(app);
 
-db.sync()
-  .then(() => {
-    console.log("Database synced");
+db.sync({ force: true }).then(() => {
+  console.log("Database synced");
 
-    server.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error(err);
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
   });
+});
--- server/utils/database.js
+++ server/utils/database.js
@@ -6,13 +6,4 @@ const sequelize = new Sequelize("example", "admin", "abc123", {
   dialect: "mysql",
 });
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Connection has been established successfully.");
-  })
-  .catch((error) => {
-    console.error("Unable to connect to the database: ", error);
-  });
-
 module.exports = sequelize;
