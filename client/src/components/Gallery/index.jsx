5a1ea31e243adc25044a01925269d8b119f11612
hafezfhmi <hafezfahmi2000@gmail.com>
2023-01-11 21:32:40 +0800

    Feat: Client - Add hover effect on gallery image

--- client/src/components/Gallery/index.jsx
+++ client/src/components/Gallery/index.jsx
@@ -82,6 +82,86 @@ let imageList = [
       "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492__340.jpg",
     user_id: 123,
   },
+  {
+    id: 1,
+    title: "Image 1",
+    description: "A beautiful landscape",
+    image_url:
+      "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492__340.jpg",
+    user_id: 123,
+  },
+  {
+    id: 1,
+    title: "Image 2",
+    description: "A beautiful landscape",
+    image_url:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIe2CBQBICvUwtDYghuUTY2LRsrrHZquSrcEk05MCIRzG-YMuXVuO-JB5cEc70QJWXYyI&usqp=CAU",
+    user_id: 123,
+  },
+  {
+    id: 1,
+    title: "Image 3",
+    description: "A beautiful landscape",
+    image_url:
+      "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492__340.jpg",
+    user_id: 123,
+  },
+  {
+    id: 1,
+    title: "Image 4",
+    description: "A beautiful landscape",
+    image_url:
+      "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492__340.jpg",
+    user_id: 123,
+  },
+  {
+    id: 1,
+    title: "Image 5",
+    description: "A beautiful landscape",
+    image_url:
+      "https://upload.wikimedia.org/wikipedia/commons/1/15/Cat_August_2010-4.jpg",
+    user_id: 123,
+  },
+  {
+    id: 1,
+    title: "Image 6",
+    description: "A beautiful landscape",
+    image_url:
+      "https://images.pexels.com/photos/10188725/pexels-photo-10188725.jpeg",
+    user_id: 123,
+  },
+  {
+    id: 1,
+    title: "Image 7",
+    description: "A beautiful landscape",
+    image_url:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNQ2biucYNrBfYWY_EVTQiWbRtbNA0sWegUCwrsuZJBOmYy1ijw3nRoJcweMWmFEGt-A0&usqp=CAU",
+    user_id: 123,
+  },
+  {
+    id: 1,
+    title: "Image 8",
+    description: "A beautiful landscape",
+    image_url:
+      "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492__340.jpg",
+    user_id: 123,
+  },
+  {
+    id: 1,
+    title: "Image 9",
+    description: "A beautiful landscape",
+    image_url:
+      "https://upload.wikimedia.org/wikipedia/commons/1/15/Cat_August_2010-4.jpg",
+    user_id: 123,
+  },
+  {
+    id: 1,
+    title: "Image 10",
+    description: "A beautiful landscape",
+    image_url:
+      "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492__340.jpg",
+    user_id: 123,
+  },
 ];
 
 const Gallery = () => {
@@ -201,28 +281,28 @@ const Gallery = () => {
     <div className={styles.gallery}>
       <div className={styles.galleryColumn + " " + styles.firstColumn}>
         {firstColumn.map((curr) => (
-          <div>
+          <div className={styles.imageContainer}>
             <img src={curr.image_url} alt="" />
           </div>
         ))}
       </div>
       <div className={styles.galleryColumn + " " + styles.secondColumn}>
         {secondColumn.map((curr) => (
-          <div>
+          <div className={styles.imageContainer}>
             <img src={curr.image_url} alt="" />
           </div>
         ))}
       </div>
       <div className={styles.galleryColumn + " " + styles.thirdColumn}>
         {thirdColumn.map((curr) => (
-          <div>
+          <div className={styles.imageContainer}>
             <img src={curr.image_url} alt="" />
           </div>
         ))}
       </div>
       <div className={styles.galleryColumn + " " + styles.fourthColumn}>
         {fourthColumn.map((curr) => (
-          <div>
+          <div className={styles.imageContainer}>
             <img src={curr.image_url} alt="" />
           </div>
         ))}
